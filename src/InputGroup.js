f39eb400da979de83be2ae5c73b5da18203cfc35
Waldemar Kunz <63851100+c0dewalker@users.noreply.github.com>
2021-06-30 12:50:33 +0200

    Add pizzaConstructor logic

---
+++ src/InputGroup.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export const InputGroup = ({ type, onChange, options, label }) => {
+    return (
+        <>
+            {label && <label>{label}</label>}
+            <form onChange={onChange}>
+                {Object.values(options).map(
+                    ({ category, value, defaultChecked, label }) => {
+                        const id = category + '-' + value
+                        return (
+                            <React.Fragment key={id}>
+                                <input
+                                    type={type}
+                                    name={category}
+                                    id={id}
+                                    value={value}
+                                    defaultChecked={defaultChecked}
+                                />
+                                <label htmlFor={id}>{label}</label>
+                            </React.Fragment>
+                        )
+                    }
+                )}
+            </form>
+        </>
+    )
+}
