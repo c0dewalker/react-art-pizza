e79a0283a27cd8cd5367995125f7abbaba4ac581
Waldemar Kunz <63851100+c0dewalker@users.noreply.github.com>
2021-06-30 12:46:27 +0200

    Add pizzaConstructor logic

---
+++ src/data.js
@@ -0,0 +1,128 @@
+export const BASE_PRICE = 200
+
+export const data = {
+    size: {
+        regular: {
+            label: '30 cm',
+            value: 'regular',
+            category: 'size',
+            price: 0,
+            defaultChecked: true
+        },
+        big: {
+            label: '35 cm',
+            value: 'big',
+            category: 'size',
+            price: 50,
+            defaultChecked: false
+        }
+    },
+    bread: {
+        fluffy: {
+            label: 'fluffy',
+            value: 'fluffy',
+            category: 'bread',
+            price: 0,
+            defaultChecked: true
+        },
+        thin: {
+            label: 'thin',
+            value: 'thin',
+            category: 'bread',
+            price: 0,
+            defaultChecked: false
+        }
+    },
+    sauce: {
+        tomato: {
+            label: 'tomato',
+            value: 'tomato',
+            category: 'sauce',
+            price: 0,
+            defaultChecked: true
+        },
+        white: {
+            label: 'white',
+            value: 'white',
+            category: 'sauce',
+            price: 0,
+            defaultChecked: false
+        },
+        hot: {
+            label: 'hot',
+            value: 'hot',
+            category: 'sauce',
+            price: 0,
+            defaultChecked: false
+        }
+    },
+    cheese: {
+        mozzarella: {
+            label: 'mozzarella',
+            value: 'mozzarella',
+            category: 'cheese',
+            price: 29,
+            defaultChecked: false
+        },
+        cheddar: {
+            label: 'cheddar',
+            value: 'cheddar',
+            category: 'cheese',
+            price: 29,
+            defaultChecked: false
+        },
+        dorBlue: {
+            label: 'dor blue',
+            value: 'dorBlue',
+            category: 'cheese',
+            price: 29,
+            defaultChecked: false
+        }
+    },
+    vegetables: {
+        tomato: {
+            label: 'tomato',
+            value: 'tomato',
+            category: 'vegetables',
+            price: 29,
+            defaultChecked: false
+        },
+        mushrooms: {
+            label: 'mushrooms',
+            value: 'mushrooms',
+            category: 'vegetables',
+            price: 29,
+            defaultChecked: false
+        },
+        paprika: {
+            label: 'paprika',
+            value: 'paprika',
+            category: 'vegetables',
+            price: 29,
+            defaultChecked: false
+        }
+    },
+    meat: {
+        bacon: {
+            label: 'bacon',
+            value: 'bacon',
+            category: 'meat',
+            price: 29,
+            defaultChecked: false
+        },
+        chicken: {
+            label: 'chicken',
+            value: 'chicken',
+            category: 'meat',
+            price: 29,
+            defaultChecked: false
+        },
+        ham: {
+            label: 'ham',
+            value: 'ham',
+            category: 'meat',
+            price: 29,
+            defaultChecked: false
+        }
+    }
+}
